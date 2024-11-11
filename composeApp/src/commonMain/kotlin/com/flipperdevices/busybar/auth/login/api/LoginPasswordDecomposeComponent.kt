ae49c882502e4ac1f6775fb9a8c45295e19d2fdb
Nikita Kulikov <n@kulikov.uk>
2024-11-11 12:35:33 +0000

    Fix compile

--- composeApp/src/commonMain/kotlin/com/flipperdevices/busybar/auth/login/api/LoginPasswordDecomposeComponent.kt
+++ composeApp/src/commonMain/kotlin/com/flipperdevices/busybar/auth/login/api/LoginPasswordDecomposeComponent.kt
@@ -3,7 +3,6 @@ package com.flipperdevices.busybar.auth.login.api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.ComponentContext
-import com.flipperdevices.busybar.auth.login.composable.LoginPasswordComposableScreen
 import com.flipperdevices.busybar.auth.login.composable.LoginPasswordScreenComposable
 import com.flipperdevices.busybar.core.decompose.DecomposeComponent
 import me.tatarka.inject.annotations.Assisted
