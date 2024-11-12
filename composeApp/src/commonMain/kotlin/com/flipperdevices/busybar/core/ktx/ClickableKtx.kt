f2a0ce9cb33510c88da0baaeb2c5a506913a5c93
Nikita Kulikov <n@kulikov.uk>
2024-11-12 15:44:08 +0000

    Update kotlin

--- composeApp/src/commonMain/kotlin/com/flipperdevices/busybar/core/ktx/ClickableKtx.kt
+++ composeApp/src/commonMain/kotlin/com/flipperdevices/busybar/core/ktx/ClickableKtx.kt
@@ -2,22 +2,21 @@ package com.flipperdevices.busybar.core.ktx
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material.ripple
 import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 
+@Composable
 fun Modifier.clickableRipple(
     bounded: Boolean = true,
     enabled: Boolean = true,
     onClick: () -> Unit
-) = composed {
-    this.then(
-        clickable(
-            interactionSource = remember { MutableInteractionSource() },
-            indication = rememberRipple(bounded = bounded),
-            onClick = onClick,
-            enabled = enabled
-        )
-    )
-}
+) = clickable(
+    interactionSource = remember { MutableInteractionSource() },
+    indication = ripple(bounded),
+    onClick = onClick,
+    enabled = enabled
+)
